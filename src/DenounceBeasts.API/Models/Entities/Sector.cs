3b23b298744f1839fcceb4f54c9333cfba9d6b4e
Starling Germosen <sgermosen@outlook.com>
2026-01-31 12:09:05 -0400

    adding relation between municipality and sector

---
+++ src/DenounceBeasts.API/Models/Entities/Sector.cs
@@ -0,0 +1,15 @@
+﻿namespace DenounceBeasts.API.Models
+{
+    public class Sector
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PostalCode { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public bool IsActive { get; set; }
+
+        public int MunicipalityId { get; set; } 
+        public Municipality Municipality { get; set; }
+    }
+}
