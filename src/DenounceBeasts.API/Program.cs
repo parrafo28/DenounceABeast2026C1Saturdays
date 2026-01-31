fee2252d98d610476dae39e9c290213c7f995fe5
Starling Germosen <sgermosen@outlook.com>
2026-01-31 11:20:07 -0400

    Adding the crud of municipality

--- src/DenounceBeasts.API/Program.cs
+++ src/DenounceBeasts.API/Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
-var key = builder.Configuration.GetSection("key");// "This is my test key for authentication";
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
